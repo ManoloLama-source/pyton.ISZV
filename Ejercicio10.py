90c766d8939139540f9501bf329156764ecf87aa
Manuel Sanchez Olivos <msanoli2503@ieszaidinvergeles.org>
2025-10-01 09:49:41 +0200

    Ejercicio10

---
+++ Ejercicio10.py
@@ -0,0 +1,7 @@
+Pesop=112
+Pesom=75 #Aqui lo que hemos echo es simplemente declarar dos variables con el peso de los payasos y las muñecas
+P=float(input("Numero de payasos del ultimo pedido: "))
+M=float(input("Numero de muñecas del ultimo pedido: "))
+
+Resultado=(P*Pesop)+(M*Pesom)
+print("El peso total del pedido es de: ",Resultado,"gramos")
