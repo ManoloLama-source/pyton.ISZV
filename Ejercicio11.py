c15dddf6a38cab190f032682800d4df3a6bbce82
Manuel Sanchez Olivos <msanoli2503@ieszaidinvergeles.org>
2025-10-01 10:10:21 +0200

    ejercicio11

--- Ejercicio11.py
+++ Ejercicio11.py
@@ -1,7 +1,7 @@
 Dinero=float(input("¿Cuanto dinero tienes en la cuenta?: "))
 
 interes= 0.04
-año=0
+año=1
 while año <= 3:
     beneficios=Dinero*(1+interes)
     print(f"El dinero que has obtenido en el año {año} es de {beneficios} euros")
