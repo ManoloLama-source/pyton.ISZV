39cd6853b5a0b7348a2cd306eceedba183df74b3
Manuel Sanchez Olivos <msanoli2503@ieszaidinvergeles.org>
2025-10-07 14:04:41 +0200

    Ejer2C

---
+++ Ejercicios-Cadenas/EjerC-2.py
@@ -0,0 +1,4 @@
+A=input("Dime un nombre: ")
+print (A.lower())
+print (A.upper())#Hay que poner los parentesis dentro de los parentesis paraq ue funcione
+print (A.title())#Pone la primera letra en mayuscula y las demas en minusculaJose 
