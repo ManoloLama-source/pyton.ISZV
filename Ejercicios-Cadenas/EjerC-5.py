fc8d355bfcb485441afb0105b93efa8732a1547a
Manuel Sanchez Olivos <msanoli2503@ieszaidinvergeles.org>
2025-10-14 14:00:20 +0200

    Actu

---
+++ Ejercicios-Cadenas/EjerC-5.py
@@ -0,0 +1,13 @@
+# Pedir al usuario que introduzca una frase
+frase = input("Introduce una frase: ")
+
+# Invertir la frase
+frase_invertida = frase[::-1]
+
+# Mostrar la frase invertida
+print("Frase invertida:", frase_invertida)
+
+#[::] significa "desde el inicio hasta el final"
+
+#[::-1] significa "desde el inicio hasta el final con paso -1" (invertido)
+
