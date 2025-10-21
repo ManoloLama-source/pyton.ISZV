d44050f5223afc28cce3964a5fb0f4e53533bd28
Manuel Sanchez Olivos <msanoli2503@ieszaidinvergeles.org>
2025-10-21 14:20:20 +0200

    Update

---
+++ Ejercicios-if/EjerIF-3.py
@@ -0,0 +1,8 @@
+Numero1=float(input("Dime un numero: "))
+Numero2=float(input("Dime otro numero: "))
+
+Division=Numero1/Numero2
+if Division==0:
+    print("Muy bien el numero no tiene resto")
+else:
+    print("Mal el numero tiene resto")
