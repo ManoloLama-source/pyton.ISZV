6e504da537991c9b2327be5874f553a37267aabb
Manuel Sanchez Olivos <msanoli2503@ieszaidinvergeles.org>
2025-10-21 14:17:16 +0200

    Update

--- Ejercicios-if/EjerIF-2.py
+++ Ejercicios-if/EjerIF-2.py
@@ -1,8 +1,9 @@
 contraseña_guardada = "Juan1234"
 contraseña_introducida = input("Introduce la contraseña: ")
 
-if contraseña_introducida.lower == contraseña_guardada.lower:#los pone en minuscula y los compara 
-                                                            #por eso es el .lower al final
+if contraseña_introducida.lower() == contraseña_guardada.lower():#los pone en minuscula y los compara 
+                                                            #por eso es el .lower al final 
+                                                            # poner el final () los parentesis parq ue funcione
     print("Las contraseñas coinciden.")
 
 else:
