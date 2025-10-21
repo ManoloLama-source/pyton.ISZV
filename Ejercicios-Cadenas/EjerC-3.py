cd4a9d53bdb99979904defb1e380ac3ac7ef3b31
Manuel Sanchez Olivos <msanoli2503@ieszaidinvergeles.org>
2025-10-21 13:39:48 +0200

    update

--- Ejercicios-Cadenas/EjerC-3.py
+++ Ejercicios-Cadenas/EjerC-3.py
@@ -1,6 +1,5 @@
 A=input("Dime un nombre:" )
-
-AES = A.replace(" ", "") #Quita los eeespacios y los convierte en nada para que no cuente los espacios que son caracyeres
+AES = A.replace(" ", "") #Quita los espacios y los convierte en nada para que no cuente los espacios que son caracyeres
 num_letras = len(AES)#La etiqueta len cuenta los caracteres y los covierte en un numero
 
 print(f"{A.upper()} tiene {num_letras} letras")
