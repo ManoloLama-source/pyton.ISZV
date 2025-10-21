af7b0dbb1d8819e62f68cf7c4a17d77457933d93
Manuel Sanchez Olivos <msanoli2503@ieszaidinvergeles.org>
2025-10-21 13:34:17 +0200

    update

---
+++ Ejercicios-Cadenas/EjerC-11.py
@@ -0,0 +1,19 @@
+
+nombre = input("Introduce el nombre del producto: ")
+precio = float(input("Introduce el precio unitario: "))
+unidades = int(input("Introduce el número de unidades: "))
+
+# Calcular el coste unitario
+coste_total = precio * unidades
+
+# 
+partes = [
+    f"{nombre}:",
+    f"{precio:6.2f} € ",#Espacio total 6, dos decimales que se ponen con .2f
+    f"Unidades: {unidades:03d} ",#Tres digitos con ceros a la izquierda si es necesario
+    f"Coste total: {coste_total:8.2f} €"
+]
+
+# Unir todo con espacios en este caso o con comas si se quiere
+resultado = " ".join(partes)
+print(resultado)
