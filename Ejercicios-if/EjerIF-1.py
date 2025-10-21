ae6c0b8a3d2e4b54fa8e9309e1ce438ebec4c94d
Manuel Sanchez Olivos <msanoli2503@ieszaidinvergeles.org>
2025-10-21 14:14:27 +0200

    Update

---
+++ Ejercicios-if/EjerIF-1.py
@@ -0,0 +1,7 @@
+Edad=int(input("Dime tu edad: "))#poner el int antes del input convierte el valor en numero entero 
+                                 #si no lo toma como cadena de texto
+                                 #float es con decimales 
+if Edad>=18:
+    print("Eres mayor de edad")
+else:
+    print("Eres menor de edad")    
