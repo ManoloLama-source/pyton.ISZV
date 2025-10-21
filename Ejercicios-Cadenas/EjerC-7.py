2128d6db5cebc8ee071bba385f883566b38ef671
Manuel Sanchez Olivos <msanoli2503@ieszaidinvergeles.org>
2025-10-21 13:04:06 +0200

    Update

--- Ejercicios-Cadenas/EjerC-7.py
+++ Ejercicios-Cadenas/EjerC-7.py
@@ -2,10 +2,10 @@
 correo = input("Introduce tu correo electrónico: ")
 
 # Dividir el correo en dos partes.
-nombre = correo.split('@')[0]
+correonuv = correo.split('@')[0]#Despues del @ se separa  
 
 # Crear el nuevo correo con dominio ceu.es
-nuevo_correo = nombre + "@ceu.es"
+nuevo_correo = correonuv + "@ceu.es"
 
 # Mostrar el resultado
 print("Tu nuevo correo es:", nuevo_correo)
