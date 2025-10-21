c411e6d1385816e5f4e0937cb05a877d17a13f61
Manuel Sanchez Olivos <msanoli2503@ieszaidinvergeles.org>
2025-10-21 14:22:08 +0200

    Update

---
+++ Ejercicios-if/EjerIF-4.py
@@ -0,0 +1,7 @@
+
+numero = int(input("Introduce un número entero: "))
+
+if numero % 2 == 0:#El operador % devuelve el resto de la division 
+    print(f"El número {numero} es par")
+else:
+    print(f"El número {numero} es impar")
