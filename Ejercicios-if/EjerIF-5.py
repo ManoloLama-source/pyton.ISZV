590d03d78ef69d2a016cc5d11e63cab5945700ef
Manuel Sanchez Olivos <msanoli2503@ieszaidinvergeles.org>
2025-10-21 14:30:15 +0200

    Update

---
+++ Ejercicios-if/EjerIF-5.py
@@ -0,0 +1,6 @@
+Edad=int(input("Introduce tu edad: "))
+Ingreso=float(input("Introduce tu ingreso anual en euros: "))
+if Edad >= 16 and Ingreso >= 1000:
+    print("Tienes que tributar.")
+else :
+    print("No tienes que tributar.") 
