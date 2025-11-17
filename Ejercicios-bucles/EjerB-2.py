a5303428955f1a345c63f1d6309ffa986252630b
msanoli2503-wq <msanoli2503@ieszaidinvergeles.org>
2025-11-17 19:31:00 +0100

    2

---
+++ Ejercicios-bucles/EjerB-2.py
@@ -0,0 +1,4 @@
+Años = int(input("Introduce un número de años: "))
+
+for i in range(1,Años + 1): #empieza desde 1 y va simando hasta la edad por eso años + 1
+        print("Usted tiene ",i, "años")
