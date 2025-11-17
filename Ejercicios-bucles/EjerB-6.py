120513111e42ed6a3aae738f8330c0f7202574d5
msanoli2503-wq <msanoli2503@ieszaidinvergeles.org>
2025-11-17 20:47:54 +0100

    6

---
+++ Ejercicios-bucles/EjerB-6.py
@@ -0,0 +1,7 @@
+altura = int(input("Introduce la altura del triángulo: "))
+
+for i in range(1, altura + 1):
+    #  se puede pultiplicar multiplicar" un string
+    # "*" * 1 = "*"
+    # "*" * 3 = "***"
+    print("*" * i)
